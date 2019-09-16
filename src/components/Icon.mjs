b4b9c7f805f7b53d269fd61809a41c10adab651e
Jayden Seric <me@jaydenseric.com>
2019-09-16 22:26:53 +1000

    Icon improvements.
    - The Icon component no longer has a color prop. Use an inline style prop instead.
    - The Icon component now sets the SVG stroke to currentColor.
    - Updated the IconTick to use a polyline with a stroke instead of a path with a fill.

--- src/components/Icon.mjs
+++ src/components/Icon.mjs
@@ -2,17 +2,15 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 export const Icon = React.forwardRef(
-  (
-    { size = '1em', color = 'currentColor', title, children, ...props },
-    ref
-  ) => (
+  ({ size = '1em', title, children, ...props }, ref) => (
     <svg width={size} height={size} viewBox="0 0 32 32" {...props} ref={ref}>
       <title>{title} icon</title>
       {children}
       <style jsx>{`
         svg {
           display: block;
-          fill: ${color};
+          stroke: currentColor;
+          fill: currentColor;
         }
       `}</style>
     </svg>
